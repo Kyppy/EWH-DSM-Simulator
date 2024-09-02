271ad9e45cf460e59b9870642aab8ca091c18e7c
Kyppy <simanikyppy@gmail.com>
2024-09-02 17:48:42 +0200

    initialise codebase

---
+++ ewh_sim.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  4 10:35:00 2023
+
+@author: Kyppy
+"""
+import math
+import random
+specific_heat_cap = 4180
+
+class EWH: 
+    def __init__(self, always_on=False, element_rating=3000, draw_rate=15, 
+                 inlet_temp=25, mass=150, randomised=False, thermal_conduct=0.341, 
+                 upper_temp_limit=60, lower_temp_limit=50, volume=150):
+        self.activation_timer = 0
+        self.always_on = always_on
+        self.current_temp = 25.2
+        self.draw_event = False
+        self.draw_rate = draw_rate/60
+        self.element_on = False
+        self.element_rating = element_rating
+        self.full_draw_duration = (volume/draw_rate) * 60
+        self.inlet_temp = inlet_temp
+        self.is_active = False
+        self.lower_temp_limit = lower_temp_limit
+        self.mass = mass
+        self.randomised = randomised
+        self.thermal_conduct = thermal_conduct
+        self.upper_temp_limit = upper_temp_limit
+        self.volume = volume
+        
+    def calculate_alpha(self, time_step):
+        return (-1*time_step)/(specific_heat_cap*self.mass*self.thermal_conduct)
+    
+    def calculate_power(self, delta_temp, time_step):
+        return ((self.mass * specific_heat_cap * delta_temp)/time_step)
+    
+    def draw_event_loss (self, time_step):
+        sigma = (self.volume - (self.draw_rate*time_step))/self.volume
+        return sigma * (self.current_temp - self.inlet_temp) + self.inlet_temp
+        
+    def increase_temp(self, time_step):
+        energy = self.element_rating * time_step
+        return (energy)/(specific_heat_cap*self.mass) + self.current_temp
+    
+    def randomise_settings(self):
+        self.element_rating = random.choice([2000, 3000, 4000])
+        self.mass = random.choice([100, 150, 200, 250])
+        self.thermal_conduct = round(random.uniform(0.30, 0.65), 2)
+        self.upper_temp_limit = round(random.uniform(56, 71), 2)
+        self.lower_temp_limit = self.upper_temp_limit - round(random.uniform(1, 5), 2)
+        self.current_temp = round(random.uniform(self.lower_temp_limit, self.upper_temp_limit), 2)
+    
+    def standing_loss (self, ambient_temperature, time_step):
+        alpha = self.calculate_alpha(time_step)
+        return ambient_temperature + ((self.current_temp-ambient_temperature)*math.exp(alpha))
+
+class Simulation:
+    def __init__(self, activation_limit=150, ambient_temperature=25.2, days=5, 
+                 draw_event_limits=(7, 20), draw_event_frequency=180, 
+                 event_timer=0, cycle_refresh_rate= 24, draw_volume=50, 
+                 event_average=8, temp_variance=False, time_step=5):
+        self.activation_limit = int((activation_limit*60)/time_step)
+        self.ambient_temperature = ambient_temperature
+        self.days = days
+        self.day_periods = int(days*24*(3600/time_step))
+        self.draw_event_durations = range(draw_event_limits[0]*12, (draw_event_limits[1]*12)+1)
+        self.draw_event_frequency = int((draw_event_frequency*60)/time_step)
+        self.draw_volume = draw_volume
+        self.event_timer = event_timer
+        self.cycle_refresh_period = (cycle_refresh_rate * 3600)/time_step
+        self.event_average = event_average
+        self.temp_variance = temp_variance
+        self.time_step = time_step
+        self.time_scale = int((3600/time_step))
+        
+    def constrained_sum_sample_pos(self, n, total):
+        """Return a randomly chosen list of n positive integers summing to total.
+        Each such list is equally likely to occur."""
+
+        dividers = sorted(random.sample(range(1, total), n - 1))
+        return [a - b for a, b in zip(dividers + [total], [0] + dividers)]
+    
+    def chunk_event_periods(self):
+        total_periods = int((self.days*86400)/self.time_step)
+        event_periods = range(total_periods)
+        day_period = int((24*60*60)/self.time_step)
+        chunked_periods = [event_periods[i:i + day_period] for i in range(0, len(event_periods), day_period)]
+        return chunked_periods
+        
+    def is_draw_period(self, time_period):
+        if time_period%self.draw_event_frequency == 0:
+            return True
+        else:
+            return False
+    
+    def refresh_event_timer (self):
+        self.event_timer += random.choice(self.draw_event_durations)
+    
+    def refresh_max_events(self, time_period):
+        if time_period%self.cycle_refresh_rate == 0:
+            return True
+        else:
+            return False
+    
+    def daily_event_distribution (self):
+        event_total = (self.days * self.event_average)
+        return self.constrained_sum_sample_pos(self.days, event_total)
+    
+    def generate_daily_random_draw_periods(self):
+        daily_event_periods = self.chunk_event_periods()
+        draw_periods = []
+        
+        for daily_period in daily_event_periods:
+            draw_periods.append(random.choice(daily_period))
+        
+        return draw_periods
+    
+    def generate_bounded_draw_periods(self, start_hour=6, end_hour=8):
+        start_period = start_hour * self.time_scale
+        end_period = end_hour*self.time_scale
+        
+        bounded_periods = []
+        for i in range(0, self.day_periods, int(24*self.time_scale)):
+            event_bounds = range(start_period + i, end_period + i)
+            bounded_periods.append(random.choice(event_bounds))
+        
+        return bounded_periods
+        
+    def generate_event_pool(self):
+        daily_event_frequency = self.daily_event_distribution()
+        event_times = self.chunk_event_periods()
+
+        event_pool = []
+        for event_frequency, event_times in zip(daily_event_frequency,event_times):
+            event_pool += random.sample(event_times, event_frequency)
+        return event_pool
+    
+    def generate_activation_timing(self):
+        event_periods = self.chunk_event_periods()
+        activation_timing = []
+        for event_period in event_periods:
+            activation_timing += random.sample(event_period, 2)
+        return activation_timing
+    
+    def generate_activation_time(self):
+        return random.choice(range(self.activation_limit))
+    
+    def generate_time_periods(self):
+        return int((self.days*86400)/self.time_step)
+    
---
+++ main.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 13:04:24 2024
+
+@author: Kyppy
+"""
+
+import ewh_sim
+import math
+import pandas as pd
+from pandas.tseries.offsets import DateOffset
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+
+def days_to_seconds(days):
+    return days * 86400
+
+def hours_to_seconds(hours):
+    return hours * 3600
+
+def generate_period(simulation_duration, time_step):
+    if simulation_duration < 1:
+        simulation_duration = 1
+    
+    return int(simulation_duration * 86400/time_step)
+
+#SIMULATION SETTINGS
+# set number of simulation days
+simulation_days = 1
+# simulation time steps defined in seconds
+time_step = 60
+# define single simulation time period
+sim_period = generate_period(simulation_days, time_step)
+# extract real temperature data from CSV file to dataframe 
+temp_profile_df = pd.read_csv("data/measured_temperatures/historical_temp_2019_2022.csv", header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col='datetime')
+temp_profile_df.index = pd.to_datetime(temp_profile_df.index)
+# define containers to contain EWH temperature(s) and power
+ewh_temp = []
+ewh_power = []
+
+
+# instantiate EWH object
+ewh = ewh_sim.EWH(always_on=True, randomised=False)
+# instantitate and initialise simulation settings and ambient temp value
+sim = ewh_sim.Simulation(days=simulation_days, temp_variance=True, time_step=time_step)
+ambient_temp = sim.ambient_temperature
+
+#RUN SIMULATION 
+for sim_day in range(simulation_days):
+    # set container for simulated EWH settings
+    ewh_settings = [ewh.mass, ewh.element_rating, ewh.upper_temp_limit, ewh.lower_temp_limit]
+        
+    # if temperature variance is active, initialise ambient EWH temp from real temp data
+    if sim.temp_variance:
+       # initialise simulation ambient temp
+       sim.ambient_temperature = temp_profile_df.iloc[0]["temperature_2m (°C)"]
+       # set the date-time stamp for the next ambient temp value
+       ambient_stamp = temp_profile_df.index[0] + DateOffset(hours=1)
+    
+    # for simulation_day in range(simulation_days):
+    # set EWH temp control to run continuously
+    if ewh.always_on:
+        ewh.is_active = True
+    
+    # set containers for simulated EWH temperature, power and event time periods
+    temp = []
+    power = []
+    activation_periods = []
+    draw_event_periods = []
+   
+    #generate EWH temperature profile
+    for period in range (sim_period):
+        #every hour update the ambient temp using real temp data
+        if sim.temp_variance and period%60==0:
+            sim.ambient_temperature = temp_profile_df.loc[ambient_stamp]["temperature_2m (°C)"]
+            ambient_stamp = ambient_stamp + DateOffset(hours=1)
+            
+        #determine temperature change due to standing losses
+        ewh.current_temp = ewh.standing_loss(sim.ambient_temperature, sim.time_step)
+        
+        #determine usage event occurence
+        # if period in draw_event_periods:
+        #     ewh.draw_event = True
+        #     sim.event_timer = ewh.full_draw_duration/sim.time_step
+        #     # sim.refresh_event_timer()
+               
+        #temperature change from usage event
+        # if ewh.draw_event and sim.event_timer > 0:
+        #     ewh.current_temp = ewh.draw_event_loss(sim.time_step)
+        #     sim.event_timer -= 1
+            
+        #     if sim.event_timer < 1:
+        #         ewh.draw_event = False
+        
+        #determine ewh activation
+        # if period in activation_periods and not ewh.always_on:
+        #     if not ewh.is_active:
+        #         ewh.is_active = True
+        #     ewh.activation_timer += sim.generate_activation_time()
+            
+        #determine temperature change due to ewh element
+        if ewh.is_active:
+            if ewh.element_on:
+                if ewh.current_temp < ewh.upper_temp_limit:
+                    ewh.current_temp = ewh.increase_temp(sim.time_step)
+                    power.append(ewh.element_rating)
+                else:
+                    ewh.element_on = False
+                    power.append(0)
+            else:
+                if ewh.current_temp < ewh.lower_temp_limit:
+                    ewh.element_on = True
+                    ewh.current_temp = ewh.increase_temp(sim.time_step)
+                    power.append(ewh.element_rating)
+                else:
+                    power.append(0)
+        else:
+            power.append(0)
+        
+        #determine remaining ewh activation time
+        # if ewh.activation_timer > 0 and not ewh.always_on:
+        #     ewh.activation_timer -= 1
+            
+        # if ewh.activation_timer < 1 and not ewh.always_on:
+        #     ewh.element_on = False
+        #     ewh.is_active = False
+            
+        temp.append(ewh.current_temp)
+    # add current EWH temperature to list of other simulated EWH temperatures
+    ewh_temp.append(temp)
+    # add current EWH power to list of other simuated EWH power
+    ewh_power.append(power)
+    
+    # # add current EWH settings to settings dataframe
+    # ewh_df.loc[len(ewh_df.index)] = ewh_settings
+
+
+#INITIALISE DATAFRAME TO STORE SIMULATION RESULTS
+# restrict number of samples when plotting results for readability
+# default is 24-hours worth of samples assuming minutely time steps
+plotting_limit = 1440
+
+# define date-time range
+# date-time starts at midnight of the first day of the year
+# minimum period is 24-hours
+sim_datetime = pd.date_range('2024-01-01 00:00:00', periods=sim_period, 
+                             freq="{0}s".format(time_step))
+# initialise and populate dataframe index and columns
+sim_df = pd.DataFrame(index=sim_datetime)
+sim_df.index.name = 'datetime'
+sim_df_column_names = ['EWH Temperature (\u2103)', 'EWH Power (kW)']
+sim_df['EWH Temperature (\u2103)'] = ewh_temp[0]
+sim_df['EWH Power (kW)'] = [power//1000 for power in ewh_power[0]]
+    
+#PLOT EWH SIMULATION RESULTS
+# figure display settings
+date_format = DateFormatter("%H:%M")
+plt.rcParams['figure.dpi'] = 300
+
+# define plot figure
+sim_fig, sim_ax = plt.subplots(1, figsize=(18, 7))
+
+#plot simulation results
+sim_ax.set_title('EWH Sim Results')
+sim_ax.set_xlabel('Time')
+sim_ax.set_ylabel('Amplitude')
+sim_ax.margins(x=0.001, y=0.02)
+sim_ax.xaxis.set_major_formatter(date_format)
+sim_ax.plot(sim_df['EWH Temperature (\u2103)'])
+sim_ax.plot(sim_df['EWH Power (kW)'])
+sim_fig.tight_layout()
+
+#SAVE SIMULATIONS TO FILE
+# save EWH temperature profiles to CSV 
+#ewh_temp_df.to_csv("training_data/simulated_profiles/{0}_day_ewh_temp_profiles.csv".format(simulation_days))
+
+# save EWH power profile to CSV 
+#ewh_power_df.to_csv("training_data/simulated_profiles/{0}_day_ewh_power_profiles.csv".format(simulation_days))
+
